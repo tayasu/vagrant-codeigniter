0f9e16477f835ea4797a47e11faf3b09e682be83
Kazuya Takahashi <takahashi@realworld.jp>
2013-02-18 15:32:13 +0900

    Edit config file

--- codeigniter/application/config/config.php
+++ codeigniter/application/config/config.php
@@ -69,7 +69,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']	= 'japanese';
 
 /*
 |--------------------------------------------------------------------------
@@ -180,7 +180,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
@@ -191,7 +191,7 @@ $config['log_threshold'] = 0;
 | application/logs/ folder. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = '/var/log/vagrant-codeigniter/';
 
 /*
 |--------------------------------------------------------------------------
